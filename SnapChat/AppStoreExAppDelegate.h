e98bb9f2bc4a95a0c0e73f206215bb7a7c8455a7
KimByungyoon <kimbyungyoon@KimByungyoon-no-MacBook-Pro.local>
2013-08-18 10:39:15 +0900

    Initial Commit

---
+++ SnapChat/AppStoreExAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppStoreExAppDelegate.h
+//  SnapChat
+//
+//  Created by KimByungyoon on 2013/08/18.
+//  Copyright (c) 2013年 KimByungyoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppStoreExAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnapChat/AppStoreExViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppStoreExViewController.h
+//  SnapChat
+//
+//  Created by KimByungyoon on 2013/08/18.
+//  Copyright (c) 2013年 KimByungyoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppStoreExViewController : UIViewController
+
+@end
